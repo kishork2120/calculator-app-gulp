b400ca36e82e05fba3516982849368c29d555e5c
@kishorspark2120 <kishorekumar@sparkouttech.com>
2021-06-15 22:30:45 +0530

    calculator functionality finished

--- js/index.js
+++ js/index.js
@@ -1,11 +1,37 @@
-// const stringmath = require('string-math');
 import stringmath from 'string-math';
+
+
+// evaluate the expression
+function eveluate(){
+  if(!document.querySelector('#expression').value) return;
+  document.querySelector('#expression').value = stringmath(document.querySelector('#expression').value);
+}
+
 window.onload = function(){
-  
+
+  document.querySelector('#expression').addEventListener('keyup',(event)=>{
+    if(event.keyCode === 13){
+      eveluate();
+    }
+  })
+
   // adding event listeners to all buttons in the page
   document.querySelectorAll('button').forEach(function(element){
     element.addEventListener('click', function(event){
-      console.log(stringmath('2+2'));
+
+      // clear the input field
+      if(event.target.innerHTML === 'CE'){
+        document.querySelector('#expression').value = '';
+        return
+      }
+
+      // evaluate the expression on pressing '='
+      if(event.target.innerHTML === '='){
+        eveluate();
+        return
+      }
+
+      document.querySelector('#expression').value = document.querySelector('#expression').value.toString()+event.target.innerHTML;
     })
   })
 }
